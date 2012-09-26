b323c8966cd6f68b081757a36d9c24d9934b631c
Kathy <kathy@cater2.me>
2012-09-26 11:28:06 -0400

    added logic for saving sub roles to is_client and is_vendor in users

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,:token_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :newsletter, :role_ids
-
+  attr_accessible :email, :password, :password_confirmation, :remember_me,
+                  :first_name, :last_name, :newsletter, :role_ids, :is_client, :is_vendor
 
   #return true if the user is not vendor or client
   def is_c2me?
@@ -21,6 +21,18 @@ class User < ActiveRecord::Base
   #return the user Role
   def role?(role)
     return !!self.roles.find_by_name(role.to_s.camelize)
+  end
+
+  def client_or_vendor(role_id)
+    client_roles = %w(employee office_manager)
+    vendor_roles = %w(vendor carrier)
+    role_name = Role.where(:id => role_id).select(:name).first.to_s
+    if client_roles.include?(role_name)
+      self.is_client = true
+    elsif vendor_roles.include?(role_name)
+      self.is_vendor = true
+    end
+    self.save
   end
 
 end
