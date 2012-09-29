05ebb5f6db2e33c6151598e4d9e064f4278567b7
Bruno <bruno@cater2.me>
2012-09-28 17:52:06 -0700

    add http basic code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  http_basic_authenticate_with :name => 'demO', :password => 'dbAcc3ss!'
+
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
   end
