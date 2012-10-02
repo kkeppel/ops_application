5be1555fb196533e78104c78009feeb9eed19b26
Bruno Serveur <bruno@cater2.me>
2012-10-02 23:03:07 +0000

    fix1

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120928194610) do
+ActiveRecord::Schema.define(:version => 20121001212356) do
 
   create_table "cities", :force => true do |t|
     t.string  "name"
@@ -32,8 +32,6 @@ ActiveRecord::Schema.define(:version => 20120928194610) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
-
-  add_index "client_profiles", ["key"], :name => "index_client_profiles_on_key"
 
   create_table "companies", :force => true do |t|
     t.string   "company_name"
@@ -72,6 +70,11 @@ ActiveRecord::Schema.define(:version => 20120928194610) do
   create_table "roles_users", :id => false, :force => true do |t|
     t.integer "role_id"
     t.integer "user_id"
+  end
+
+  create_table "staffs", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "states", :force => true do |t|
